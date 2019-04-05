797d89fc6f82a744ebd2d6c884a860d0fb0c2cbf
tozawashou <afgab>
2019-04-05 22:08:42 +0900

    必須のバリデーションを追加

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,2 +1,6 @@
 class Monster < ApplicationRecord
+  validates :name, presence: true
+  validates :level, presence: true
+  validates :attack, presence: true
+  validates :defense, presence: true
 end
