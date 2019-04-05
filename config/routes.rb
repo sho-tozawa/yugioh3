17a4e77894fb94935641e3c8588fa0e04e2e5c63
tozawashou <afgab>
2019-04-05 21:50:08 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  get 'top' => 'home#top'
-  get 'monsters' => 'monsters#index'
-  get 'monsters/new' => 'monsters#new'
-  get 'monsters/show/:id' => 'monsters#show'
-  post 'monsters' => 'monsters#create'
-  get 'monsters/:id/edit' => 'monsters#edit'
-  post 'monsters/:id/edit' => 'monsters#update'
-  post 'monsters/:id/destroy' => 'monsters#destroy'
+  resources :monsters
 
-  get 'monsters/:id' => 'monsters#show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
